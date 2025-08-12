0c5a8a838cacf803c280de281ad49486e4d6bff1
Francisco Padrino <37840747+fpadrino9@users.noreply.github.com>
2025-08-12 13:05:52 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
-import "bootstrap";
-import "./style.css";
 
+const number = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+const palos= ["&spades;","&clubs;","&hearts;","&diams;"];
+const colorPalo = ["negro","rojo"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
+const generarcolorRandom = () => {
+  return indexNumbers = Math.floor(Math.random() *colorPalo.length); 
+}
+const generarNumeroRandom = () => {
+  return indexNumbers = Math.floor(Math.random() * number.length); 
+}
+const generarPaloRandom = () => {
+  return indexSuit = Math.floor(Math.random() * palos.length);
+}
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let posPalo = generarNumeroRandom();
+  let posNumero = generarPaloRandom();
+  let poscolor = generarcolorRandom()
+  console.log(colorPalo[poscolor]);
+  const card =document.querySelector(".card");
+  const h1 = document.createElement('h1')
+  h1.classList.add("h1");
+  h1.innerHTML = number[posPalo];
+  card.appendChild(h1);
+  const palo1 = document.createElement('h1')
+  palo1.classList.add("top-suit")
+  palo1.classList.add(colorPalo[poscolor]);
+  palo1.innerHTML = palos[posNumero];
+  card.appendChild(palo1);
+
+  const palo2 = document.createElement('h1')
+  palo2.classList.add("bottom-suit")
+  palo2.innerHTML = palos[posNumero];
+  card.appendChild(palo2);
+  palo2.classList.add(colorPalo[poscolor]);
+
+
+
+  
+
 };
